906c0e199bbc512a94e24fc3643965ce67012ebe
MarEf <maria.efimova93@gmail.com>
2023-03-13 16:09:51 +0200

    Removed backend test console logs

--- server/auth.js
+++ server/auth.js
@@ -7,13 +7,11 @@ module.exports = async (req, res, next) => {
     try {
         // Get token from authorization headers
         const token = await req.headers.authorization
-        console.log("Token " + token)
         // Check if the token provided matches the supposed origin
-        const verify = jwt.verify(
+        jwt.verify(
             token,
             process.env.RANDOM_TOKEN
         )
-        console.log("Verification: " + JSON.stringify(verify))
         next()
 
     } catch (error) {
